a044999778f883b484796bfe8c128521ae33da15
Jeff Cimmino <jeffc@bhrcis.com>
2014-10-06 08:54:35 -0400

    Initial Commit

---
+++ expense-calculator/Project3/Source.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout << "7 + 3 =" << 7 + 3 << endl;
+	cout << "50 - 10000 =" << 50 - 10000 << endl; 
+	cout << "5 x 123854 =" << 5 * 123854 << endl;
+	cout << "103 / 918236 =" << 103 / 918236 << endl;
+	cout << "1.498273/ 8.165897 =" << 1.498273 / 8.165897 << endl;
+	cout << "958 % 132 =" << 958 % 132 << endl;
+	cout << "7 + 3 * 5 =" << 64 + 123 * 0 << endl;
+	cout << "(7 + 3) * 5 =" << (3 + 3) * 5 << endl;
+	cout << "The #include function includes a library for use." << endl;
+	cout << "using namespace std forces use of the standard library eliminating the need to type std:: in front of cout and cin." << endl;
+	cout << "cout << allows you to show text to the console" << endl;
+	cout << "endl starts a new line." << endl;
+	cout << "cin >> allows the user to provide input." << endl;
+	cout << "system('PAUSE') is a system function that pauses console output." << endl;
+	cout << "return 0 allows the program to exit out with a positive code." << endl;
+	cout << "C++ has basic math built in, and you can extend math functionality by typing #include <math.h>.  Am I dismissed?" << endl;
+	system("PAUSE");
+	return 0;
+}
